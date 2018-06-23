bc359a915e8aa260bbc9d4b81e0898ca9051ab61
Gustavo <gustavo@pop-os.localdomain>
2018-06-23 15:10:27 -0300

    Novo spawner
    mudaça na dinomica do spawner, menos matematica mais visual

--- models/spawner/Spawner.gd
+++ models/spawner/Spawner.gd
@@ -1,32 +1,33 @@
 extends Position2D
 
-onready var enemy = load("res://characters/test_enemy/test_enemy.tscn")
-var enemy_count = 50
-
-onready var level = $"../PlayerHandler/Player/Camera2D".get_viewport_rect()
 export (float) var minWaitTime
 export (float) var maxWaitTime
 export (NodePath) var Handler 
+
+onready var enemy = load("res://characters/test_enemy/test_enemy.tscn")
+onready var Player = $"../PlayerHandler".get_child(0) 
 onready var EnemiesHandler = get_node(Handler)
- 
+onready var Spawns = $"../TileMap".get_children()
+onready var VisibleSpawns = []
+
+var enemy_count = 0
+
 func _ready():
 	randomize()
 	$"Timer".set_wait_time(rand_range(minWaitTime,maxWaitTime))
 	$"Timer".start()
 
 func _on_Timer_timeout():
-	var Player = $"../PlayerHandler".get_child(0) 
+	var sort = rand_range(0,VisibleSpawns.size())
+	var new_enemy = enemy.instance()
+	EnemiesHandler.get_parent().add_child(new_enemy)
+	var new_position = VisibleSpawns[sort]
+	var wd = new_position.get_rect().size.x * new_position.get_scale().x
+	var enemy_shape = new_enemy.get_node("CollisionShape2D").shape.get_extents().y
 
-	if enemy_count > 0:
-		var new_enemy = enemy.instance()
-		var cells = $"../Floor".get_used_cells()
-		var sort = rand_range(0,cells.size())
-		var new_position =  cells[sort] * 16
-		
-		EnemiesHandler.get_parent().add_child(new_enemy)
-		new_enemy.set_position(Vector2(new_position.x + 8,new_position.y - new_enemy.get_node("CollisionShape2D").shape.get_extents().y))
-		enemy_count -= 1
-
+	new_enemy.set_position(Vector2(new_position.global_position.x + rand_range(0,wd),new_position.global_position.y - enemy_shape))
+	print(Performance.get_monitor(0),"/",enemy_count)
+	enemy_count += 1
 	$"Timer".set_wait_time(rand_range(minWaitTime,maxWaitTime))
 	$"Timer".start()
 	pass 
